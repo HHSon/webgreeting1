ad58152d046763e5afb1ae934051d053d09cfeed
HHSon <sonhoanghuuson@yahoo.com.vn>
2014-08-24 18:00:11 +0700

    learn for release tc-log-import - try 2

---
+++ src/main/java/test/TestExample.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author sonhh
+ */
+public class TestExample extends TestCase {
+    @Test
+    public void add() {;
+        Assert.assertFalse(true);
+    }
+}
